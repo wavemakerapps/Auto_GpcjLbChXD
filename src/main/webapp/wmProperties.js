a14a254030568387ae0252eb1fbef9acd4f1bcf4
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-24 06:24:28 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_GpcjLbChXD",
+  "homePage" : "Main",
+  "name" : "Auto_GpcjLbChXD",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
